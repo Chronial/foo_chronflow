104884ed49de473a0f1f1434c654e86a33ac7d03
Chronial <git@chronial.de>
2019-05-13 20:12:52 +0200

    Fix texture border handling

--- Image.cpp
+++ Image.cpp
@@ -248,6 +248,8 @@ GLTexture UploadReadyImage::upload() const {
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 16);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
   GLint glInternalFormat;
   if (cfgTextureCompression) {
