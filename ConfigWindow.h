84a401a6c238157295a392d23aeb5f4ba624ab63
DaYuYu <foodayuyu@gmail.com>
2020-09-30 22:01:00 +0200

    Also default source and target folders are now equal Resolved issue with source playlist combobox initialization to a non-existant folder Re-Enabled Context menu option 'Follow Library selections'
    Signed-off-by: DaYuYu <foodayuyu@gmail.com>

--- ConfigWindow.h
+++ ConfigWindow.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 // clang-format off
 #include <windowsx.h>
 
@@ -230,7 +230,7 @@ class BehaviourTab : public ConfigTab {
       {IDC_CHECK_BEHA_ACTFLAG_E_RATED_HIGHT, 2, ACT_RATED_HIGHT}  // flag 6
       // still place for another flag (8 in total)
   };
-
+  
   UINT action_block_combo[nblock] = {IDC_DOUBLE_CLICK, IDC_MIDDLE_CLICK, IDC_ENTER_KEY};
 
   // note: another operation available (4 in total)
@@ -287,7 +287,7 @@ class BehaviourTab : public ConfigTab {
     while (x >>= 1) ++targetlevel;
     return targetlevel;
   }
-  //todo: still unsure about these flags/reqs/UI
+  //todo: still unsure about these flags/reqs/UI  
   void InitFlagCheckBoxes(HWND hwnd, pfc::string8* blocklist, unsigned long initflags) {
     std::vector<std::vector<byte>> vallflags;
     ActionFlagsToArray(vallflags, initflags);
@@ -438,7 +438,7 @@ class BehaviourTab : public ConfigTab {
             textChanged(IDC_HIDDEN_CUSTOM_ACTION_FLAG);
           }
         } else if (HIWORD(wParam) == CBN_SELCHANGE) {
-
+          
           // check if an action selection combobox triggered the event
 
           if (const int block = isActionComboBox(LOWORD(wParam)); block > -1) {
@@ -453,7 +453,7 @@ class BehaviourTab : public ConfigTab {
             }
 
             // update flag checkboxes: enabled/disabled state for custom action
-
+            
             bool bshow = CustomAction::isCustomAction(g_customActions, selected, true);
             for (int itflag = 0; itflag < ncheckboxflag; itflag++) {
               UINT idcontrol =
@@ -526,7 +526,7 @@ class BehaviourTab : public ConfigTab {
       uSendMessageText(list, CB_INSERTSTRING, 0, action->actionName);
     }
     int itemId = uSendDlgItemMessageText(hWnd, id, CB_FINDSTRINGEXACT, 1, selectedItem);
-    itemId = uSendDlgItemMessageText(hWnd, id, CB_SETCURSEL, itemId, 0);
+    itemId = uSendDlgItemMessageText(hWnd, id, CB_SETCURSEL, itemId, 0); 
     SetWindowRedraw(list, true);
   }
 
@@ -594,44 +594,44 @@ class SourcesTab : public ConfigTab {
 
   void loadComboSourcePlayList(UINT id, const char* selectedItem) {
     std::map<t_size, pfc::string8> playlistmap;
-
-    //typedef service_ptr_t<class playlist_window> playlist_window_ptr;
-    //bool bres = playlist_manager::get()->enumerate().next(p_out);
+    
+    bool bdefault_playlist_exists =
+        (pfc_infinite != playlist_manager::get()->find_playlist(default_SourcePlaylistName));
+    int DefId;
 
     HWND list = GetDlgItem(hWnd, id);
     SetWindowRedraw(list, false);
     SendMessage(list, CB_RESETCONTENT, 0, 0);
     uSendMessageText(list, CB_ADDSTRING, 0, "");
     uSendDlgItemMessage(hWnd, id, CB_RESETCONTENT, 0, 0);
-    int DefId =
-    uSendDlgItemMessageText(hWnd, id, CB_ADDSTRING, 0, default_SourcePlaylistName);
-    //uSendDlgItemMessageText(hWnd, id, CB_SELECTSTRING, 1, default_SourcePlaylistName);
+
+    if (!bdefault_playlist_exists)
+      DefId = uSendDlgItemMessageText(hWnd, id, CB_ADDSTRING, 0, default_SourcePlaylistName);
 
     t_size cplaylist = playlist_manager::get()->get_playlist_count();
     for (int i=0;i<cplaylist;i++) {
       pfc::string8 aname;
       t_size pl = playlist_manager::get()->playlist_get_name(i, aname);
       int rowId = uSendDlgItemMessageText(hWnd, id, CB_ADDSTRING, 0, aname);
+      if (stricmp_utf8(default_SourcePlaylistName, aname) == 0) {
+        DefId = rowId;
+      }
       uSendDlgItemMessage(hWnd, id, CB_SETITEMDATA, rowId, i);
       if (uSendDlgItemMessage(hWnd, id, CB_GETCURSEL, 0, 0) == CB_ERR) {
         if (stricmp_utf8(default_SourcePlaylistName, selectedItem) == 0) {
           uSendDlgItemMessageText(hWnd, id, CB_SETCURSEL, DefId, 0);
-        } else if (stricmp_utf8(aname.c_str(), selectedItem) == 0) {
+        } else
+        if (stricmp_utf8(aname.c_str(), selectedItem) == 0) {
           rowId = uSendDlgItemMessageText(hWnd, id, CB_FINDSTRINGEXACT, 1, selectedItem);
           rowId = uSendDlgItemMessageText(hWnd, id, CB_SETCURSEL, rowId, 0);
         }
       }
     }
-
+    //fix the combo if still undefined
     int checkId = uSendDlgItemMessage(hWnd, id, CB_GETCURSEL, 0, 0);
     if (checkId == CB_ERR) {
-      if (stricmp_utf8(selectedItem, default_SourcePlaylistName) != 0) {
-        uSendDlgItemMessageText(hWnd, id, CB_SETCURSEL, DefId, 0);
-        pfc::string8 title;
-        title << "Playlist not found";
-        uMessageBox(hWnd, "Default Playlist Source not found\nValue has been reset",
-                    title, MB_APPLMODAL | MB_OK | MB_ICONINFORMATION);
-      }
+      int rowId = uSendDlgItemMessageText(hWnd, id, CB_ADDSTRING, 0, selectedItem);
+      uSendDlgItemMessageText(hWnd, id, CB_SETCURSEL, rowId, 0);
     }
 
     SetWindowRedraw(list, true);
@@ -697,7 +697,7 @@ class DisplayTab : public ConfigTab {
             hWnd, IDC_FRAME_WIDTH_SPIN, UDM_SETRANGE, 0, MAKELONG(short(30), short(0)));
         uSetDlgItemInt(
           hWnd, IDC_FRAME_WIDTH, configData->HighlightWidth, false);
-
+        
         break;
       }
       case WM_HSCROLL: {
@@ -718,7 +718,7 @@ class DisplayTab : public ConfigTab {
 
         //will trigger EN_CHANGE & therefore textchanged(id)
         uSetDlgItemText(hWnd, idtextdoublecontrol, std::to_string(val * 0.01).c_str());
-
+        
       } break;
       case WM_DRAWITEM: {
         auto* drawStruct = reinterpret_cast<DRAWITEMSTRUCT*>(lParam);
@@ -849,7 +849,7 @@ class DisplayTab : public ConfigTab {
             case IDC_TEXTCOLOR_CUSTOM:
             case IDC_FONT_CUSTOM:
             case IDC_BG_COLOR_CUSTOM:
-            case IDC_COVER_ART_PNG8_ALPHA:
+            case IDC_COVER_ART_PNG8_ALPHA: 
             case IDC_COVER_DISPLAY_LEGACY_EXTVIEWER: {
               buttonClicked(LOWORD(wParam));
 
@@ -1213,7 +1213,7 @@ class CoverTab : public ConfigTab {
             GetCoverConfigPosition(configData->CoverConfigs, next->first.c_str());
         HWND ctrl = GetDlgItem(hWnd, IDC_SAVED_SELECT);
         int oldselection_ndx = uSendMessage(ctrl, CB_GETCURSEL, 0, 0);
-
+        
         m_edited_name = oldselection;
         m_edited_ndx = oldselection_ndx;
         //refresh combobox list
@@ -1291,7 +1291,7 @@ class CoverTab : public ConfigTab {
         selection_ndx = itemId;
         selection_name = name.c_str();
         uSendDlgItemMessageText(hWnd, IDC_SAVED_SELECT, CB_SELECTSTRING, 1, name.c_str());
-      }
+      } 
     }
 
     if (selection == -1 || selection_ndx == -1) {
@@ -1511,7 +1511,7 @@ class ControlsTab : public ConfigTab {
         DeleteObject(editBoxFont);
       case WM_COMMAND:
         if (HIWORD(wParam) == EN_CHANGE) {
-        }
+        } 
         break;
     }
     return FALSE;
@@ -1560,7 +1560,7 @@ class ConfigWindow {
   HWND GetCurrentTabWnd() {
     return GetTabWindow(GetCurrentTab());
   }
-
+  
   ConfigTab* GetCurrentConfigTab() {
     ConfigTab* ct = tabs[GetCurrentTab()].get();
     return ct;
