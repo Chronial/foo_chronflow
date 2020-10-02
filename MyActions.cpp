fc9df413e7fa848a000193503e4c9dc8cfb8c8f2
DaYuYu <foodayuyu@gmail.com>
2020-10-02 22:18:28 +0200

    Custom Actions flags revised
    Signed-off-by: DaYuYu <foodayuyu@gmail.com>

--- MyActions.cpp
+++ MyActions.cpp
@@ -1,87 +1,427 @@
 #include "MyActions.h"
 
-#include "config.h"
+#include "ConfigData.h"
+
+using coverflow::configData;
+using coverflow::CoverConfigMap;
+
+void CustomAction::NotifyRejected(HWND hwnd) {
+  // MessageBeep(0xFFFFFFFF);
+  // SendMessage(hwnd, WM_MYACTIONS_CANCELED, 0, 0);
+}
+
+bool CustomAction::availablePlaylist(t_size playlist) {
+  bool bres = true;
+  if (playlist == ~0)
+    bres = false;
+  else if (playlist_manager::get()->playlist_lock_is_present(playlist))
+    bres = false;
+  return bres;
+}
+
+void CustomAction::DoPlayItem(t_size p_item, t_size playlist,
+                              const pfc::bit_array_bittable& p_mask, int flag) {
+
+  t_size p_inspos = p_mask.find_first(true, 0, p_mask.size());
+
+  if (p_inspos == pfc_infinite) return; //nothing to do
+
+  static_api_ptr_t<playlist_manager> pm;
+  t_size uiplaylist = ~0;
+  bool b_need_queue = false;
+  bool b_target_not_active = false;
+
+  uiplaylist = pm->get_active_playlist();
+  b_target_not_active = (uiplaylist != playlist);
+
+  if (!BlockFlagEnabled(flag, ACT_ACTIVATE))
+    b_need_queue = b_target_not_active;
+
+  metadb_handle_list tracks;
+
+  try {
+      pm->playlist_get_items(playlist, tracks, p_mask);
+      if (tracks.get_count() > 0) //todo: test exceptions for next line...
+          pm->playlist_set_focus_by_handle(playlist, tracks[0]);
+      else
+          return;
+  }
+  catch (std::exception e) {
+  }
+
+  if (!b_need_queue) {
+    pfc::bit_array_bittable playmask(p_mask.size());
+    if (tracks.get_count() == 1) {
+      // one track in mask for play menu command
+      GUID commandGuid;
+      if (menu_helpers::find_command_by_name("Play", commandGuid)) {
+        menu_helpers::run_command_context(commandGuid, pfc::guid_null, tracks);
+      }
+    } else
+      pm->playlist_execute_default_action(playlist, p_inspos);
+  } else {
+    // We queue
+  }
+}
+
+// returns start position for new items, -1 for no changes
+t_size CustomAction::DoAddReplace(const pfc::list_base_const_t<metadb_handle_ptr>& tracks,
+                                  t_size playlist, int flag, t_size lastselected,
+                                  pfc::list_t<metadb_handle_ptr>& updateTracks,
+                                  bit_array_bittable& updateMask) {
+
+  bool bres = false;
+  updateTracks = tracks;
+
+  static_api_ptr_t<playlist_manager> pm;
+
+  // previous item position
+  int position;
+  t_size prevLength = pm->playlist_get_item_count(playlist);
+
+  t_size playlistPlaying;
+  t_size itemPlaying;
+  bool wasPlaying;
+  bool wasPlayingValid = pm->get_playing_item_location(&playlistPlaying, &itemPlaying);
+
+  if (wasPlayingValid) {
+    wasPlaying = (playlistPlaying == playlist);
+    itemPlaying = wasPlaying ? itemPlaying : ~0;
+  } else
+    wasPlaying = false;
+
+  t_size itemFocused = pm->playlist_get_focus_item(playlist);
+
+  // insert position priority: after last selected, after focus, after currently playing
+  // otherwise append
+
+  if (lastselected != ~0) {
+    position = lastselected;
+  } else if (itemFocused != ~0) {
+    position = itemFocused;
+  } else if (wasPlaying) {
+    position = itemPlaying;
+  } else  // not playing, no selection and no item focused
+    position = prevLength - 1;
+
+  bool b_ADD = BlockFlagEnabled(flag, ACT_ADD);
+  bool b_INS = BlockFlagEnabled(flag, ACT_INSERT);
+  bool b_end_position = (position == prevLength - 1);
+
+  // updateMask
+  if (!b_INS && !b_ADD) {
+    // REPLACE
+    position = -1;
+    pm->playlist_clear(playlist);
+    updateMask.resize(tracks.get_count());
+    for (int i = 0; i < tracks.get_count(); i++) updateMask.set(i, true);
+  } else {
+    if ((!b_INS && b_ADD) || (b_INS && b_end_position)) {
+      // ADD
+      position = prevLength - 1;
+      for (int i = prevLength; i <= position + tracks.get_count(); i++)
+        updateMask.set(i, true);
+    } else {
+      if (b_INS && !b_ADD) {
+        // INSERT
+        int pending = tracks.get_size();
+        for (int i = 0; i < updateMask.size(); i++) {
+          if (i <= position || i >= position + tracks.get_count() + pending)
+            updateMask.set(i, false);
+          else {
+            updateMask.set(i, true);
+            pending--;
+          }
+          if (i == position)
+            pending++;
+        }
+      } else {
+        if (b_INS && b_ADD)
+          ;  // not implemented
+      }
+    }
+  }
+  // end updateMask
+
+  // rating
+  bool b_rated = BlockFlagEnabled(flag, ACT_RATED);
+  bool b_best = BlockFlagEnabled(flag, ACT_RATED_HIGHT);
+
+  if (b_rated || b_best) {
+    pfc::list_t<metadb_handle_ptr> ratedTracks;
+    int count_rated = GetOnlyRated(b_best, updateTracks, ratedTracks, updateMask);
+    if (count_rated != updateTracks.get_count()) {
+      updateTracks = ratedTracks;
+    };
+  }
+
+  // backup and send (after previous item position)
+  t_size t_pos = position + 1;
+  t_size t_res = pfc_infinite;
+  if (updateTracks.get_count() > 0) {
+    pm->playlist_undo_backup(playlist);
+    t_res = pm->playlist_insert_items(playlist, t_pos, updateTracks, bit_array_false());
+  }
+  return t_res;
+}
+
+int CustomAction::GetOnlyRated(bool onlyhighrates,
+                               const pfc::list_base_const_t<metadb_handle_ptr>& tracks,
+                               pfc::list_t<metadb_handle_ptr>& rated_items,
+                               bit_array_bittable& updateMask) {
+
+  int pending = tracks.get_size();
+  int masksize = updateMask.size();
+  bit_array_bittable ratedMask(masksize);
+  int count_unrated = 0;
+  for (t_size walk = updateMask.find_first(true, 0, masksize); walk < masksize;
+       walk = updateMask.find_next(true, walk, masksize)) {
+    metadb_handle_ptr atrack = tracks.get_item(tracks.get_size() - pending);
+    pending--;
+    if (CheckRate(atrack, onlyhighrates) > -1) {
+      ratedMask.set(walk - count_unrated, true);
+      rated_items.add_item(atrack);
+    } else {
+      count_unrated++;
+    }
+  }
+  ratedMask.resize(masksize - count_unrated);
+  updateMask.resize(ratedMask.size());
+  for (int i = 0; i < updateMask.size(); i++)
+    updateMask.set(i, ratedMask.get(i));
+
+  return rated_items.get_count();
+}
+
+// track rates
+// return values: 0 rated track, -1 not rated or
+// track rate or -1 if hrate enabled
+int CustomAction::CheckRate(metadb_handle_ptr track, bool hrate) {
+  int res;
+  service_ptr_t<titleformat_object> script;
+  pfc::string8 format = "%rating%";
+
+  if (static_api_ptr_t<titleformat_compiler>()->compile(script, format)) {
+    static_api_ptr_t<playback_control> pbc;
+    pbc->playback_format_title_ex(
+        track, nullptr, format, script, nullptr, playback_control::display_level_titles);
+    if (stricmp_utf8(format.get_ptr(), "?") != 0) {
+      if (hrate) {
+        try {
+          res = std::stoi(format.get_ptr());
+          res = (res >= configData->CustomActionHRate ? res : -1);
+        } catch (...) {
+          res = -1;
+        }
+      } else
+        res = 0; // track is rated
+    } else {
+      res = -1;
+    }
+  } else
+    res = -1;
+  return res;
+}
+//... send to playlist
+bool CustomAction::SendToPlaylist(const pfc::list_base_const_t<metadb_handle_ptr>& tracks,
+                                  t_size playlist, int flag) {
+  if (playlist == pfc_infinite)
+    return false;
+
+  static_api_ptr_t<playlist_manager> pm;
+
+  //get prev selection
+  t_size prevLength = pm->playlist_get_item_count(playlist);
+  bit_array_bittable prevSelectionMask(prevLength);
+  pm->playlist_get_selection_mask(playlist, prevSelectionMask);
+  t_size lastSelected = ~0;
+  for (t_size walk = prevSelectionMask.find_first(true, 0, prevLength);
+       walk < prevLength;
+       walk = prevSelectionMask.find_next(true, walk, prevLength)) {
+    lastSelected = walk;
+  }
+
+  pfc::list_t<metadb_handle_ptr> updateTracks;
+  bit_array_bittable updateMask(prevLength + tracks.get_count());
+
+  //add insert replace & check rates
+  t_size changeposition =
+      DoAddReplace(tracks, playlist, flag, lastSelected, updateTracks, updateMask);
+
+  if (BlockFlagEnabled(flag, ACT_ACTIVATE)) {
+    if (changeposition != pfc_infinite) {
+      pm->set_active_playlist(playlist);
+      pm->playlist_ensure_visible(playlist, changeposition);
+      pm->set_playing_playlist(playlist);
+    }
+  }
+
+  if (BlockFlagEnabled(flag, ACT_PLAY)) {
+    if (changeposition != pfc_infinite) {
+      DoPlayItem(0, playlist, updateMask, flag);
+    }
+  }
+
+  if (BlockFlagEnabled(flag, ACT_HIGHLIGHT)) {
+    if (changeposition != pfc_infinite) {
+      // set bit_array_false to OR selection with updateMask
+      // set bit_array_true to AND updateMask with selection
+      pm->playlist_set_selection(playlist, bit_array_true(), updateMask);
+    }
+  }
+  return (changeposition != pfc_infinite);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
 
 namespace {
-class AddToPlaylist : public CustomAction {
+
+class TargetActivePlaylist : public CustomAction {
  public:
-  AddToPlaylist() : CustomAction("Add to Active Playlist ") {}
+  TargetActivePlaylist() : CustomAction("Send to Active Playlist ", true) {}
+  void sendconfig(HWND) {};
   void run(const pfc::list_base_const_t<metadb_handle_ptr>& tracks,
-           const char* /*albumTitle*/) override {
+           const char* /*albumTitle*/, HWND hwnd, int flag) override {
+
     static_api_ptr_t<playlist_manager> pm;
-    pm->activeplaylist_undo_backup();
-    t_size oldLength = pm->activeplaylist_get_item_count();
-    pm->activeplaylist_add_items(tracks, bit_array_true());
-    pm->activeplaylist_set_selection(
-        bit_array_true(), bit_array_range(oldLength, tracks.get_count()));
-    pm->activeplaylist_ensure_visible(oldLength + tracks.get_count() - 1);
-    pm->activeplaylist_set_focus_item(oldLength);
-  }
-};
-class ReplacePlaylist : public CustomAction {
- public:
-  ReplacePlaylist() : CustomAction("Replace Active Playlist ") {}
-  void run(const pfc::list_base_const_t<metadb_handle_ptr>& tracks,
-           const char* /*albumTitle*/) override {
-    static_api_ptr_t<playlist_manager> pm;
-    pm->activeplaylist_undo_backup();
-    pm->activeplaylist_clear();
-    pm->activeplaylist_add_items(tracks, bit_array_true());
     t_size playlist = pm->get_active_playlist();
-    pm->set_playing_playlist(playlist);
-    static_api_ptr_t<playback_control> pc;
-    pc->start();
+
+    // check exists, locked or conflicts with source playlist mode
+    if (!availablePlaylist(playlist)) {
+      NotifyRejected(hwnd);
+      return;
+    }
+
+    bool bdone = SendToPlaylist(tracks, playlist, flag);
+
+    if (!bdone)
+      NotifyRejected(hwnd);
   }
 };
+
+class TargetDefaultPlaylist : public CustomAction {
+ public:
+  TargetDefaultPlaylist() : CustomAction("Send to Default Playlist ", true) {}
+  void sendconfig(HWND) {};
+  void run(const pfc::list_base_const_t<metadb_handle_ptr>& tracks,
+           const char* /*albumTitle*/, HWND hwnd, int flag) override {
+
+    static_api_ptr_t<playlist_manager> pm;
+    t_size playlist = pm->find_playlist(configData->TargetPlaylist);
+
+    if (playlist == ~0) {
+      // create playlist
+      playlist = pm->create_playlist(configData->TargetPlaylist, ~0u, ~0u);
+    } else {
+      bool b_locked = playlist_manager::get()->playlist_lock_is_present(playlist);
+      if (b_locked) {
+        NotifyRejected(hwnd);
+        return;
+      }
+    }
+
+    bool bdone = SendToPlaylist(tracks, playlist, flag);
+
+    if (!bdone)
+      NotifyRejected(hwnd);
+  }
+};
+
 class NewPlaylist : public CustomAction {
  public:
-  NewPlaylist() : CustomAction("Add to Album Playlist ") {}
+  NewPlaylist() : CustomAction("Send to Album Playlist ", true) {}
+  void sendconfig(HWND){};
   void run(const pfc::list_base_const_t<metadb_handle_ptr>& tracks,
-           const char* albumTitle) override {
+           const char* albumTitle, HWND hwnd, int flag) override {
     static_api_ptr_t<playlist_manager> pm;
     t_size playlist = pm->find_playlist(albumTitle);
-    if (playlist != ~0) {
-      pm->playlist_undo_backup(playlist);
-      pm->playlist_clear(playlist);
-    } else {
+
+    if (playlist == ~0) {
+      // create playlist
       playlist = pm->create_playlist(albumTitle, ~0u, ~0u);
-    }
-    pm->playlist_add_items(playlist, tracks, bit_array_true());
-    pm->set_active_playlist(playlist);
-    pm->set_playing_playlist(playlist);
-    static_api_ptr_t<playback_control> pc;
-    pc->start();
-  }
-};
-class TargetPlaylist : public CustomAction {
- public:
-  TargetPlaylist() : CustomAction("Replace Default Playlist ") {}
-  void run(const pfc::list_base_const_t<metadb_handle_ptr>& tracks,
-           const char* /*albumTitle*/) override {
-    static_api_ptr_t<playlist_manager> pm;
-    t_size playlist = pm->find_playlist(cfgTargetPlaylist);
-    if (playlist != ~0) {
-      pm->playlist_undo_backup(playlist);
-      pm->playlist_clear(playlist);
     } else {
-      playlist = pm->create_playlist(cfgTargetPlaylist, ~0u, ~0u);
+      bool b_locked = playlist_manager::get()->playlist_lock_is_present(playlist);
+      if (b_locked) {
+        NotifyRejected(hwnd);
+        return;
+      }
     }
-    pm->playlist_add_items(playlist, tracks, bit_array_true());
-    pm->set_active_playlist(playlist);
-    pm->set_playing_playlist(playlist);
-    static_api_ptr_t<playback_control> pc;
-    pc->start();
+
+    bool bdone = SendToPlaylist(tracks, playlist, flag);
+
+    if (!bdone)
+      NotifyRejected(hwnd);
   }
 };
-};  // namespace
+
+//pre:   -1  action activates visualization defined in post parameter
+//      >-1  action will toggle visualization pre and post
+//post:  -1  sets default visualization
+
+class SetDisplayConfig : public CustomAction {
+ public:
+  // Action name "Set Display" or "Toggle Display" + config indexes
+  SetDisplayConfig(int pre, int post) : CustomAction("", false), m_pre(pre), m_post(post) {
+
+    const pfc::string8 str_set_display_action = "Set Display";
+    const pfc::string8 str_toggle_display_action = "Toggle Display";
+
+    if (pre == -1) {
+      actionName = str_set_display_action.c_str(); //"Set Display Ndx"
+      actionName = actionName << pfc::string8(" ") << (post == -1 ? "to Default" : pfc::string8(pfc::toString(post).c_str()));
+    }
+    else {
+      actionName = str_toggle_display_action.c_str(); //"Toggle Display 1..Ndx or 1..default"
+      actionName = actionName << pfc::string8(" ") << pfc::string8(pfc::toString(pre).c_str());
+      actionName = actionName << ".." << (post == -1? "default" : pfc::string8(pfc::toString(post).c_str()));
+    }
+    bPlaylistAction = false;
+    m_pre = pre;
+    m_post = post;
+  }
+
+  void run(const pfc::list_base_const_t<metadb_handle_ptr>& tracks,
+    const char* /*albumTitle*/, HWND hwnd, int flag) {};
+  void sendconfig(HWND hwnd) override {
+    int currentpos = configData->sessionCompiledCPInfo.get().first;
+    int defaultpos = configData->GetCCPosition();
+    int ndx_pre = m_pre; // -1 for set only action, other for swap  action pre and post
+    int ndx_post = m_post == -1 ? defaultpos : m_post;
+    // set to post or toggle to post
+    if ((m_pre == -1 && currentpos != ndx_post) ||
+        (m_pre !=-1 && currentpos == ndx_pre) ||
+        (m_pre !=-1 && currentpos != ndx_pre && currentpos != ndx_post)) {
+      ndx_pre = currentpos != ndx_pre ? currentpos : ndx_pre;
+      if (currentpos != ndx_post)
+        SendMessage(hwnd, WM_MYACTIONS_SET_DISPLAY, ndx_pre, ndx_post);
+    }
+    else {
+      //toggle to pre
+      if (currentpos != ndx_pre)
+        SendMessage(hwnd, WM_MYACTIONS_SET_DISPLAY, currentpos, ndx_pre);
+    }
+  }
+
+ private:
+  int m_pre;
+  int m_post;
+};
+
+}  // namespace
 
 std::vector<CustomAction*> g_customActions{
-    new AddToPlaylist, new ReplacePlaylist, new TargetPlaylist, new NewPlaylist};
+    new TargetActivePlaylist, new TargetDefaultPlaylist, new NewPlaylist,
+    new SetDisplayConfig(-1,1), new SetDisplayConfig(0, -1)};
 
-void executeAction(const char* action, const AlbumInfo& album) {
+void executeAction(const char* action, const ::db::AlbumInfo& album, HWND hwnd,
+                   int flag) {
+
   for (auto& g_customAction : g_customActions) {
     if (stricmp_utf8(action, g_customAction->actionName) == 0) {
-      g_customAction->run(album.tracks, album.title.c_str());
+      if (g_customAction->isPlaylistAction())
+        g_customAction->run(album.tracks, album.title.c_str(), hwnd, flag);
+      else
+        g_customAction->sendconfig(hwnd);
       return;
     }
   }
--- MyActions.h
+++ MyActions.h
@@ -1,16 +1,136 @@
 #pragma once
 #include "DbAlbumCollection.h"
+#include "DbAlbumInfo.h"
+
+#define WM_MYACTIONS_CANCELED (WM_USER + 1000)
+#define WM_MYACTIONS_SET_DISPLAY (WM_USER + 1001)
 
 class CustomAction {
  protected:
-  explicit CustomAction(const char* actionName) { this->actionName = actionName; }
+  explicit CustomAction(const char* actionName, bool isPlaylistAction) {
+                        this->actionName = actionName;
+                        this->bPlaylistAction = isPlaylistAction;
+  }
+  bool availablePlaylist(t_size playlist);
+
+  void NotifyRejected(HWND hwnd);
+  void DoPlayItem(t_size p_item, t_size playlist,
+                  const pfc::bit_array_bittable& p_mask, int flag);
+
+  t_size DoAddReplace(const pfc::list_base_const_t<metadb_handle_ptr>& tracks,
+                      t_size playlist, int flag, t_size lastselected,
+                      pfc::list_t<metadb_handle_ptr>& updateTracks,
+                      bit_array_bittable& updateMask);
+
+  int GetOnlyRated(bool onlyhighrates,
+                   const pfc::list_base_const_t<metadb_handle_ptr>& tracks,
+                   pfc::list_t<metadb_handle_ptr>& rated_items,
+                   bit_array_bittable& updateMask);
+
+  int CheckRate(metadb_handle_ptr track, bool hrate);
+
+  bool SendToPlaylist(const pfc::list_base_const_t<metadb_handle_ptr>& tracks,
+                      t_size playlist, int flag);
 
  public:
+
   pfc::string8 actionName;
+  bool bPlaylistAction;
+
   virtual void run(const pfc::list_base_const_t<metadb_handle_ptr>& tracks,
-                   const char* albumTitle) = 0;
+                   const char* albumTitle, HWND engineWnd, int flag) = 0;
+  virtual void sendconfig(HWND mwnd) = 0;
+
+  bool isPlaylistAction() { return bPlaylistAction;}
+
+  static bool isCustomAction(std::vector<CustomAction*> g_customActions, const char* actionname, bool playlistaction) {
+    bool bres = false;
+    for (auto action : boost::adaptors::reverse(g_customActions)) {
+      if (action->isPlaylistAction() == playlistaction) {
+        if (stricmp_utf8(action->actionName, actionname) == 0) {
+          bres = true;
+          break;
+        }
+      }
+    }
+    return bres;
+  }
 };
 
 extern std::vector<CustomAction*> g_customActions;
 
-void executeAction(const char* action, const AlbumInfo& album);
+void executeAction(const char* action, const ::db::AlbumInfo& album,
+                   HWND enginewnd, int flag);
+
+
+byte const nblock = 3;         // custom actions blocks (double click, middle click & enter)
+byte const nflag = 8;          // flags
+byte const ncheckboxflag = 5;  // ui checkboxes for flags
+
+enum ActionBlocks {
+  AB_DOUBLECLICK = 0,
+  AB_MIDDLECLICK,
+  AB_ENTER,
+};
+
+enum ActionFlags {
+  ACT_PLAY = 1 << 0,           // 1 Play
+  ACT_ACTIVATE = 1 << 1,       // 2 Activate (always on)
+  ACT_ADD = 1 << 2,            // 4 Add or replace
+  ACT_HIGHLIGHT = 1 << 3,      // 8 Hightlight changes
+  ACT_INSERT = 1 << 4,         // 16 Insert after selected, playing...
+  ACT_RATED = 1 << 5,          // 32 Only rated
+  ACT_RATED_HIGHT = 1 << 6,    // 64 Only rated over n
+  ACT_TO_DO = 1 << 7           // 128 Not implemented
+};
+
+static inline void ActionFlagsToArray(std::vector<std::vector<byte>>& vec,
+                                      unsigned long allflags) {
+  vec.resize(nblock, std::vector<byte>(nflag, 0));
+  for (int itblock = 0; itblock < nblock; itblock++) {
+    uint8_t block_flag = itblock == 0 ? allflags : allflags >> (1 << (itblock + 2));
+    for (int itflag = 0; itflag < nflag; itflag++) {
+      uint8_t flag_mask = 1 << itflag;
+      if ((block_flag & flag_mask) == flag_mask)
+        vec[itblock][itflag] = 1;
+    }
+  }
+}
+
+static inline unsigned long ActionFlagsCalculate(int action_block, int actionflag,
+                                                 bool flagisndx,
+                                                 unsigned long customflags, bool enable) {
+  uint8_t action_block_flag_mask;
+
+  if (flagisndx)
+    action_block_flag_mask = 1 << actionflag;
+  else
+    action_block_flag_mask = actionflag;
+
+  unsigned long replaceval = action_block_flag_mask;
+  if (action_block > 0) {
+    replaceval <<= (1 << (action_block + 2));
+  }
+  if (enable)
+    customflags |= replaceval;
+  else
+    customflags &= ~(replaceval);
+
+  return customflags;
+}
+
+static inline uint8_t ActionGetBlockFlag(unsigned long customflags, int abEvent) {
+  if (abEvent > nblock)
+    return 0;
+
+  int block = abEvent;
+  uint8_t block_flag = block == 0 ? customflags : customflags >> (1 << (block + 2));
+  return block_flag;
+}
+
+static inline bool BlockFlagEnabled(uint8_t block_flag, ActionFlags aflag) {
+  if ((block_flag & aflag) == aflag)
+    return true;
+  else
+    return false;
+}
