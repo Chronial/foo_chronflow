61879bc250fcf66b71a08f70019f8875d074162e
fooDayuyu@gmail.com <foodayuyu@gmail.com>
2021-01-23 21:34:56 +0100

    Release 0 5 2-mod-preview5

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "ConfigGuids.h"
 
-#define VERSION "0.5.2-mod-preview4"
+#define VERSION "0.5.2-mod-preview5"
 
 // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
 DECLARE_COMPONENT_VERSION(component_NAME, VERSION,
