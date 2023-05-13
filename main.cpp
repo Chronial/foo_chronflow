61a0a498bf2d878b9dc3b5d0d43cb3494497a673
fooDayuyu@gmail.com <foodayuyu@gmail.com>
2023-05-13 18:49:46 +0100

    Add fb2k SDK version/target to About component

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@
 // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
 DECLARE_COMPONENT_VERSION(COMPONENT_NAME_LABEL, FOO_CHRONFLOW_VERSION,
                           "Renders Album Art in a 3d environment\n"
-                          "Version: " FOO_CHRONFLOW_VERSION "\n"
                           "Module: " COMPONENT_NAME "\n"
+                          "Version: " FOO_CHRONFLOW_VERSION "\n"
                           "Compiled: "__DATE__ "\n"
+                          "fb2k SDK: "PLUGIN_FB2K_SDK"\n"
                           "Mod by DaYuyu\n"
                           "Original Component by Christian Fersch\n"
                           "Cover image by figure002 (http://fav.me/d2ppgwx)\n");
